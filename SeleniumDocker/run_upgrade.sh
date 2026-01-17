4d3ccdac95a2d85b17f31498f76aaac24849d98a
Saurav Singh <saurav@singh.com>
2026-01-17 06:32:01 +0000

    Updated upgrade.sh path

--- SeleniumDocker/run_upgrade.sh
+++ SeleniumDocker/run_upgrade.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# 1. Get the directory where THIS script is located
+PARENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
+
+# 2. Change the current working directory to that path
+cd "$PARENT_PATH"
+
+# 3. Now these relative calls will always find the files
+echo "Starting upgrades in $PARENT_PATH..."
+
 # Run upgrade.sh for Chrome
 ./upgrade.sh chrome
+
 # Run upgrade.sh for ChromeDriver
 ./upgrade.sh chromedriver
+
+# Push changes
 git push origin main
