18a703a16a9f4fb1ecfa7eb1df6494dacc88ce02
Saurav Singh <singh.saurav@icloud.com>
2026-03-21 19:28:38 +0530

    added selPractice

---
+++ SeleniumPractice/src/test/java/com/saurav/BrowserFactory.java
@@ -0,0 +1,18 @@
+package com.saurav;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserFactory {
+
+    public static WebDriver getDriver(String browser) {
+        return switch (browser.toLowerCase()) {
+            case "chrome" -> new ChromeDriver();
+            case "firefox" -> new FirefoxDriver();
+            case "edge" -> new EdgeDriver();
+            default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
+        };
+    }
+}
---
+++ SeleniumPractice/src/test/java/com/saurav/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.saurav;
+
+import org.openqa.selenium.WebDriver;
+
+public class HelloWorld {
+    static void main() {
+        WebDriver driver = BrowserFactory.getDriver("firefox");
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
