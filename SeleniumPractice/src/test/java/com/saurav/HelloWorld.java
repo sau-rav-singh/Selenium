dc85ffc2db23ec9fcfb2bc542cb62bd922028a29
Saurav Singh <singh.saurav@icloud.com>
2026-03-26 17:24:54 +0530

    added selPractice

--- SeleniumPractice/src/test/java/com/saurav/HelloWorld.java
+++ SeleniumPractice/src/test/java/com/saurav/HelloWorld.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class HelloWorld {
 
     static void main() {
-        WebDriver driver = BrowserFactory.getDriver("firefox");
+        WebDriver driver = BrowserFactory.getDriver("edge");
         driver.get("https://www.google.com/");
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
