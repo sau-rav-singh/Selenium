2a9d313336880e530e38c3ecf3a49abcca7705d1
Saurav Singh <singh.saurav@icloud.com>
2026-04-23 19:56:52 +0530

    added StreamsTest

--- SeleniumPractice/src/test/java/tests/ECommerceTest.java
+++ SeleniumPractice/src/test/java/tests/ECommerceTest.java
@@ -2,14 +2,13 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.ECommercePage;
 import utils.ConfigReader;
 import utils.TestBase;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ECommerceTest extends TestBase {
 
@@ -27,12 +26,36 @@ public class ECommerceTest extends TestBase {
     }
 
     @Test
-    public void topDealsSortTest(){
+    public void topDealsSortTest() {
         commonActions().goTo("https://rahulshettyacademy.com/seleniumPractise/#/offers");
-        Select pageSizeSelect=new Select(commonActions().findElement(By.id("page-menu")));
-        pageSizeSelect.selectByValue("10");
-        commonActions().click(By.className("sort-icon"));
-        List<String> sortedProductName= commonActions().findElements(By.xpath("//tbody/tr/td[1]")).stream().map(WebElement::getText).toList();
-        Assert.assertEquals(sortedProductName,sortedProductName.stream().sorted().toList(),"Products should be sorted");
+        commonActions().selectByValue(By.id("page-menu"), "20");
+        commonActions().click(By.xpath("//span[text()='Veg/fruit name']"));
+        List<WebElement> elementList = commonActions().findElements(By.xpath("//tbody/tr/td[1]"));
+        List<String> originalList = elementList.stream().map(WebElement::getText).toList();
+        List<String> sortedList = originalList.stream().sorted().toList();
+        commonActions().assertEquals(originalList, sortedList, "Verify that vegetables are sorted alphabetically in the UI");
+    }
+
+    @Test
+    public void getPriceTest() {
+        commonActions().goTo("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        commonActions().selectByValue(By.id("page-menu"), "5");
+        String productName = "Wheat";
+        Optional<WebElement> productRow;
+        do {
+            productRow = commonActions().findElements(By.xpath("//tbody/tr/td[1]")).stream().filter(nameElement -> nameElement.getText().equalsIgnoreCase(productName)).findFirst();
+        }
+        while (!productRow.isPresent()) {
+            commonActions().click(By.xpath("//a[@aria-label='Next']"));
+            productRow = commonActions().findElements(By.xpath("//tbody/tr/td[1]")).stream().filter(nameElement -> nameElement.getText().equalsIgnoreCase(productName)).findFirst();
+        }
+        commonActions().assertEquals(productRow.isPresent(), true, "Verify product '" + productName + "' is present on the page.");
+
+        String price;
+        if (productRow.isPresent()) {
+            price = productRow.get().findElement(By.xpath("./following-sibling::td[1]")).getText();
+            commonActions().assertEquals(!price.isEmpty(), true, "Verify price for '" + productName + "' is not empty.");
+            System.out.println("Price of " + productName + ": " + price);
+        }
     }
 }
