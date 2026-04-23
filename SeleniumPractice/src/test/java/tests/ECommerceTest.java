6a556d643a4aa6a6e417966b880ce0e821d22a0f
Saurav Singh <singh.saurav@icloud.com>
2026-04-23 19:15:22 +0530

    added StreamsTest

--- SeleniumPractice/src/test/java/tests/ECommerceTest.java
+++ SeleniumPractice/src/test/java/tests/ECommerceTest.java
@@ -1,9 +1,15 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.ECommercePage;
 import utils.ConfigReader;
 import utils.TestBase;
+
+import java.util.List;
 
 public class ECommerceTest extends TestBase {
 
@@ -18,5 +24,15 @@ public class ECommerceTest extends TestBase {
         String[] productArray = new String[]{"tomato", "cucumber"};
         eCommercePage.addProductToCart(productPurchased);
         eCommercePage.addProductToCart(productArray);
+    }
+
+    @Test
+    public void topDealsSortTest(){
+        commonActions().goTo("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        Select pageSizeSelect=new Select(commonActions().findElement(By.id("page-menu")));
+        pageSizeSelect.selectByValue("10");
+        commonActions().click(By.className("sort-icon"));
+        List<String> sortedProductName= commonActions().findElements(By.xpath("//tbody/tr/td[1]")).stream().map(WebElement::getText).toList();
+        Assert.assertEquals(sortedProductName,sortedProductName.stream().sorted().toList(),"Products should be sorted");
     }
 }
