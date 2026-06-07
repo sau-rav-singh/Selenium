0e96ab4f4e9300bdb32cd901052d4d93e07c170f
Saurav Singh <singh.saurav@icloud.com>
2026-06-07 19:57:44 +0530

    added BaseClientPage

--- SeleniumPractice/src/test/java/pages/BaseClientPage.java
+++ SeleniumPractice/src/test/java/pages/BaseClientPage.java
@@ -4,12 +4,16 @@ import utils.CommonActions;
 
 public class BaseClientPage {
     protected final CommonActions commonActions;
+    private MainMenuSection mainMenuSection;
 
-    public BaseClientPage(CommonActions commonActions){
-        this.commonActions=commonActions;
+    public BaseClientPage(CommonActions commonActions) {
+        this.commonActions = commonActions;
     }
 
-    public MainMenuSection mainMenuSection(){
-        return new MainMenuSection(commonActions);
+    public MainMenuSection mainMenuSection() {
+        if (mainMenuSection == null) {
+            mainMenuSection = new MainMenuSection(commonActions);
+        }
+        return mainMenuSection;
     }
 }
