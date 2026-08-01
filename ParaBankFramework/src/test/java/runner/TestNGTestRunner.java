de5ddadb08ad6fb71824274dd6e1a7a28859ea26
Saurav Singh <singh.saurav@icloud.com>
2026-08-01 19:11:23 +0530

    added parabank readme

--- ParaBankFramework/src/test/java/runner/TestNGTestRunner.java
+++ ParaBankFramework/src/test/java/runner/TestNGTestRunner.java
@@ -10,7 +10,7 @@ import org.testng.annotations.DataProvider;
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
     @Override
-    @DataProvider()
+    @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
     }
